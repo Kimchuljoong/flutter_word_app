ba2fe9121015b61da7bb15636567eac5fed9a362
Kimchuljoong <wnmh123@gmail.com>
2024-02-22 21:32:08 +0900

    db config

--- lib/databaseConfig.dart
+++ lib/databaseConfig.dart
@@ -1,13 +1,14 @@
 import 'package:path/path.dart';
 import 'package:sqflite/sqflite.dart';
+import 'package:word_app/word.dart';
 
 class DatabaseService {
-  static final DatabaseService _database = DatabaseService._interval();
+  static final DatabaseService _database = DatabaseService._internal();
   late Future<Database> database;
 
   factory DatabaseService() => _database;
 
-  DatabaseService._interval() {
+  DatabaseService._internal() {
     databaseConfig();
   }
 
@@ -23,6 +24,73 @@ class DatabaseService {
       return true;
     } catch (err) {
       print(err.toString());
+      return false;
+    }
+  }
+
+  Future<bool> insertWord(Word word) async {
+    final Database db = await database;
+    try {
+      db.insert(
+        'words',
+        word.toMap(),
+        conflictAlgorithm: ConflictAlgorithm.rollback,
+      );
+      return true;
+    } catch (err) {
+      return false;
+    }
+  }
+
+  Future<List<Word>> selectWords() async {
+    final Database db = await database;
+    final List<Map<String, dynamic>> data = await db.query('words');
+
+    return List.generate(
+        data.length,
+        (index) => Word(
+              id: data[index]['id'],
+              name: data[index]['name'],
+              meaning: data[index]['meaning'],
+            ));
+  }
+
+  Future<Word> selectWord(int id) async {
+    final Database db = await database;
+    final List<Map<String, dynamic>> data =
+        await db.query('words', where: "id = ?", whereArgs: [id]);
+    return Word(
+      id: data[0]['id'],
+      name: data[0]['name'],
+      meaning: data[0]['meaning'],
+    );
+  }
+
+  Future<bool> updateWord(Word word) async {
+    final Database db = await database;
+    try {
+      db.update(
+        'words',
+        word.toMap(),
+        where: "id = ?",
+        whereArgs: [word.id],
+      );
+      return true;
+    } catch (err) {
+      return false;
+    }
+  }
+
+  Future<bool> deleteWord(int id) async {
+    final Database db = await database;
+    try {
+      db.delete(
+        'words',
+        where: "id = ?",
+        whereArgs: [id],
+      );
+      return true;
+    } catch (err) {
       return false;
     }
   }
